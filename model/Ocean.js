376109301f0401a8806ac2f797b627193697c8ac
miremi9 <56270462+miremi9@users.noreply.github.com>
2024-12-06 03:14:47 +0100

    poisson mort

--- model/Ocean.js
+++ model/Ocean.js
@@ -1,20 +1,31 @@
-import Poisson from './Poisson.js';
+import Poisson, { Saumon, Truite } from './Poisson.js';
 
 export default class Ocean {
-    constructor(nbPoissons = 10) {
+    constructor(nbPoissons = 10,ecosysteme) {
         this.oceanElement = document.getElementById("ocean");
-
+        this.ecosysteme = ecosysteme
         this.poissons = [];
-        for (let i = 0; i < nbPoissons; i++) {
-            const poisson = new Poisson(this.oceanElement);
+        for (let i = 0; i < (nbPoissons/2); i++) {
+            const poisson = new Truite(this.oceanElement,this);
             poisson.element.onclick = () => poisson.handleClick();
             this.poissons.push(poisson);
         }
+        for (let i = 0; i < (nbPoissons/2); i++) {
+            const poisson = new Saumon(this.oceanElement,this);
+            poisson.element.onclick = () => poisson.handleClick();
+            this.poissons.push(poisson);
+        }       
 
     }
     update()
     {
+        
         this.poissons.forEach(poisson => poisson.move());
+    }
+    SendPoisson(t)
+    {
+       
+        this.ecosysteme.ajouterPoisson(t,1)
     }
 }
 
--- model/ecosysteme.js
+++ model/ecosysteme.js
@@ -1,24 +1,53 @@
-import { Truite } from "./Poisson.js";
+import { Truite, Saumon, Bar, Thon, PoissonRouge, Poisson } from "./Poisson.js";
 
 class Ecosysteme {
     constructor(parent) {
-        this.poissons = new Map();
+        this.poissons = new Map([["truite", 0], ["saumon", 0], ["bar", 0], ["thon", 0], ["poissonRouge", 0]]);
+        this.dico = new Map()
+        this.dico.set('truite', new Truite(null));
+        this.dico.set('saumon', new Saumon(null));
+        this.dico.set('bar', new Bar(null));
+        this.dico.set('thon', new Thon(null));
+        this.dico.set('poissonRouge', new PoissonRouge(null));
+
+        this.predationTruite = 0.01
+
     }
 
-    ajouterPoisson(type, number = 1) {
-        if (this.poissons.has(type)) {
-            this.poissons.set(type, this.poissons.get(type) + number);
+    ajouterPoisson(nom, number = 1) {
+        
+        if (this.poissons.has(nom)) {
+            this.poissons.set(nom, this.poissons.get(nom) + number);
         } else {
-            this.poissons.set(type, number);
+            this.poissons.set(nom, number);
         }
     }
 
     update() {
+
         for (const [key, value] of this.poissons.entries()) { // Use for...of
-            this.poissons.set(key, value + 2); // Use set to update Map
-            console.log(key);
-            console.log(value);
+            this.poissons.set(key, value); // Use set to update Map
         }
+        //console.log(this.dico["truite"])
+        //this.predation(this.poissons["truite"],this.poissons["saumon"],this.dico["truite"].GetPredation(),this.dico["truite"].GetReproduction(),this.dico["saumon"].GetReproduction())
+    }
+
+
+    predation(predateur,proid,tauxPredation,reproduction)
+    {
+        let predation = tauxPredation * predateur * proid;
+
+        let pertesProid = Math.min(predation, proid);
+        proid -= pertesProid;
+
+        let nouvellesPredateur = this.tauxReproductionPredateur * predateur;
+        predateur += nouvellesPredateur;
+
+        let nouveauxProid = this.tauxReproductionProid * proid;
+        proid += nouveauxProid;
+
+        // Affichage des résultats après une mise à jour
+        console.log(`Proid: ${Math.round(predateur)}, Predateur: ${Math.round(proid)}`);
     }
 
     getNbPoisson() {
